ae0e54ab9d347978ff26ed8261eba90c353add4a
FlorinaVatamanu <75433701+FlorinaVatamanu@users.noreply.github.com>
2022-07-13 08:21:40 +0300

    Add files via upload

---
+++ models/meal-plan.ts
@@ -0,0 +1,14 @@
+export interface MealPlan {
+  id: string,
+  description: string,
+  meal_1: string,
+  snack_1: string,
+  meal_2: string,
+  snack_2: string,
+  meal_3: string,
+  snack_3: string,
+  kcals: string,
+  proteins: string,
+  carbohydrates: string,
+  fats: string
+}
