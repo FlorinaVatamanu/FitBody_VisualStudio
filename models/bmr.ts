f963a0db49ab859eca93fa86e34e9586db37493b
FlorinaVatamanu <75433701+FlorinaVatamanu@users.noreply.github.com>
2022-07-23 17:09:39 +0300

    Add files via upload

---
+++ models/bmr.ts
@@ -0,0 +1,13 @@
+export interface Bmr {
+  id: string,
+  email: string | null,
+  gender: string,
+  weight: string,
+  height: string,
+  age: string,
+  activity_level: string,
+  kcals: string,
+  carbohydrates: string,
+  fats: string,
+  proteins: string
+}
--- models/user-profile.ts
+++ models/user-profile.ts
@@ -2,9 +2,13 @@ export interface ProfileUser {
   uid: string;
   email?: string;
   displayName?: string;
+  password?: string;
+  role?: string;
   photoURL?: string;
   firstName?: string;
   lastName?: string;
   phone?: string;
   address?: string;
+  objective?: string;
+  medical_issues?: string;
 }
